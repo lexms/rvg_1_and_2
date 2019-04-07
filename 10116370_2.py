795f228dc7bd6b46fca3b6fe4449ac47a2541169
lexms <alexandermanuel.s@gmail.com>
2019-04-07 23:47:03 +0700

    second

--- 10116370_2.py
+++ 10116370_2.py
@@ -1,14 +1,14 @@
 # NIM : 10116370
 # Nama : Alexander M S
 # Kelas : MOSI-8
+import math
 
-class Simulate_case_1:
+class Simulate_case_2:
     def __init__(self):
         self.a = 7
         self.m = 128
         self.z0 = 12357
-        self.min = 800000
-        self.max = 1000000
+        self.beta = 0.1
 
     def multiplicative_formula(self,zi_minus_1):
         return (self.a * zi_minus_1) % self.m
@@ -16,8 +16,8 @@ class Simulate_case_1:
     def ui_generate(self, zi):
         return zi / self.m
 
-    def x_generate(self, ui):
-        return (self.min + ( (self.max - self.min) * ui) )
+    def t_generate(self,ui):
+        return -1*self.beta*math.log(ui)
 
     def multiplicative_list(self):
         _list = []
@@ -25,24 +25,24 @@ class Simulate_case_1:
             if i == 0:
                 zi = self.multiplicative_formula(self.z0)
                 ui = self.ui_generate(zi)
-                x = self.x_generate(ui)
-                _list.append({'Zi-1': self.z0, 'Zi' : zi, 'Ui' : ui, 'X': x})
+                t = self.t_generate(ui)
+                _list.append({'Zi-1': self.z0, 'Zi' : zi, 'Ui' : ui, 'T': t})
             elif i>0:
                 zi_minus_1 = self.multiplicative_formula(_list[i-1]['Zi-1'])   
                 zi = self.multiplicative_formula(zi_minus_1)
                 ui = self.ui_generate(zi)
-                x = self.x_generate(ui)
-                _list.append({'Zi-1': zi_minus_1, 'Zi' : zi, 'Ui' : ui, 'X': x})
+                t = self.t_generate(ui)
+                _list.append({'Zi-1': zi_minus_1, 'Zi' : zi, 'Ui' : ui, 'T': t})
 
         return _list
 
     def print_table(self):
         _list = self.multiplicative_list()
-        print('No.\t | Zi\t\t | Ui\t\t | x\t\t')
+        print('No.\t | Zi\t\t | Ui\t\t | T\t\t')
         print('--------------------------------------------------------------------------')
         for i in range(0,len(_list)):
-            print("| {}\t | {:.4f} \t | {:.4f} \t | {:.4f} \t |".format(i+1, _list[i]['Zi'], _list[i]['Ui'], _list[i]['X']))
+            print("| {}\t | {:.4f} \t | {:.4f} \t | {:.4f} \t |".format(i+1, _list[i]['Zi'], _list[i]['Ui'], _list[i]['T']))
 
-test = Simulate_case_1()
+test = Simulate_case_2()
 
 test.print_table()
